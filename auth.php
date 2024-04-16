6f63d0aa4982cbae676021ef29890858deb4f834
xJustFox <dodydaversa@gmail.com>
2024-04-16 21:35:40 +0200

    [Upgrade] Aggiunta pagina di login e bottone logout

---
+++ auth.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+include 'db_connection.php';
+
+// Prendi dati dal form di login
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Query per ottenere l'hash della password dell'utente
+$sql = "SELECT * FROM users WHERE username='$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $hash = $row['password'];
+    // Verifica se la password inserita dall'utente corrisponde all'hash memorizzato nel database
+    if (password_verify($password, $hash)) {
+        // Password corretta, autentica l'utente
+        $_SESSION['username'] = $username;
+        header("Location: ./public/index.php"); // Redirect alla dashboard
+    } else {
+        // Password non corretta, mostra un messaggio di errore
+        echo "Credenziali non valide.";
+    }
+} else {
+    // L'utente non esiste, mostra un messaggio di errore
+    echo "Credenziali non valide.";
+}
+
+$conn->close();
+?>
---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <form action="auth.php" method="POST"> 
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username"><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
